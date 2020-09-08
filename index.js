182d30dcd0a42a3a11e30171206dcce01ca81a4d
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-09-08 17:29:48 -0500

    make Procfile

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res)=> res.json({message: 'API FUNCIONANDO'}))
+
 app.use(routes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
