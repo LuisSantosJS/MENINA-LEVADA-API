199312b392e6600fc8d0f9698bfb392f068c566c
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-09-08 15:48:37 -0500

    ajsutes finos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./src/routes');
 const cors = require('cors');
 const app = express();
-const port = 3333;
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
