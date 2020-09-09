46c040e44522b11f0c24be4c2bbf847c954e8db3
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-09-08 20:36:51 -0500

    make Procfile

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const routes = require('./src/routes');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors({ origin: true }));
 
 app.get('/', (req, res)=> res.json({message: 'API FUNCIONANDO'}))
 
