a96e6f82f34b76a7e8fe32e8fdef34351921e355
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-10-18 12:39:28 -0500

    finish

--- src/database/migrations/003_create_historic.js
+++ src/database/migrations/003_create_historic.js
@@ -2,7 +2,7 @@
 exports.up = async (knex) => {
     return knex.schema.createTable('historic', table => {
         table.increments('id').primary().unique();
-        table.float('nome_cliente',255).notNullable();
+        table.string('nome_cliente',255).notNullable();
         table.string('localidade',255).notNullable();
         table.string('code', 255).notNullable();
         table.string('date', 255).notNullable();
